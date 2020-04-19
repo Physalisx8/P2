141760a9cfc6815fcd3df31f4dba9ccd632e0063
Francie <Francie.Roessler@web.de>
2020-04-19 10:27:36 +0200

    Hausaufgabe2
    Hey Stephie, ich hatte ja das Problem mit den Farben und konnte die nächsten Aufgaben nicht so richtig lösen, da ich nicht mehr herausgefunden habe warum der Regex falsch war.  Ich hoffe das ist nicht so wild.. mir hätten die Farben da echt weiter geholfen, glaube ich. Ich hab sogar noch ein BrowserTool bemüht, bin aber trotzdem nicht auf die Lösung gekommen. Ich hoffe meine Lösungen sind aus der excersiveSaveState.bin ersichtlich.
    
     Liebe Grüße Francie

---
+++ 04Vorlesung/regextool/RegexMain.java
@@ -0,0 +1,10 @@
+package regextool;
+
+public class RegexMain {
+
+	public static void main(String[] args) {
+		
+		ExerciseController controller = new ExerciseController();
+		controller.init();
+	}
+}
